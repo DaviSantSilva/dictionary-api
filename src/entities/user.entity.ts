4e52fe9a40906366d149f152fd1a0417bccd60da
Davi Silva <davi200123@gmail.com>
2025-05-11 02:09:14 -0300

    Implementation of the User Entity
    - Creation of the User entity with TypeORM
    - Configuration of TypeORM for the entity
    - Creation of the UsersModule
    - Implementation of the UsersService
    - Addition of DTOs and interfaces for validation
    - Security configuration with bcrypt for passwords

---
+++ src/entities/user.entity.ts
@@ -0,0 +1,31 @@
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+import { Exclude } from 'class-transformer';
+import { IUser } from '../modules/users/interfaces/user.interface';
+
+@Entity('users')
+export class User implements IUser {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  name: string;
+
+  @Column({ unique: true })
+  email: string;
+
+  @Column()
+  @Exclude()
+  password: string;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+} 
